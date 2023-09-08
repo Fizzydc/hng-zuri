970d3a5f66cc65d19dd458ae1237b0d329f5171d
faruq <muideenfaruq709@gmail.com>
2023-09-08 11:50:41 +0100

    update some line

---
+++ app.js
@@ -0,0 +1,19 @@
+const date = new Date()
+
+const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+const today = days[date.getDay()-1]
+
+
+
+const updateTime = () => {
+  const date = new Date()
+  timeUtc.textContent = date.getTime()
+}
+
+const todayTag = document.getElementById('today')
+const timeUtc = document.getElementById('utc')
+
+todayTag.textContent=today
+
+setInterval(updateTime,1)
